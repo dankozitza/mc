ff6c90627808a1238acbb5e43d34b40149e6fe03
Dan Kozitza <dankoz@gmx.us>
2015-06-21 10:14:11 -0500

    built matches function

--- src/tools.cpp
+++ src/tools.cpp
@@ -8,6 +8,7 @@
 #include "tools.hpp"
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 using namespace std;
 
@@ -30,7 +31,7 @@ void tools::get_vfnmake_conf(unordered_map<string, string>& config) {
 			continue;
 
 		// get the key
-		int colon_i = key.find(":");
+		unsigned colon_i = key.find(":");
 		if (colon_i == 0 || colon_i == string::npos)
 			continue;
 		key.resize(colon_i);
@@ -45,4 +46,68 @@ void tools::get_vfnmake_conf(unordered_map<string, string>& config) {
 	}
 
 	fh.close();
+}
+
+bool tools::matches(map<string, string>& results, string s, string str_re) {
+
+	results = map<string, string>();
+
+	smatch sm;
+	regex re;
+	try {
+		regex rete (str_re.c_str());
+		re.assign(rete);
+	}
+	catch (regex_error& e){
+		cout << "tools::matches: regex returned error code: `";
+	  	cout << e.code() << "` when evaluating expression: `" << str_re << "`\n";
+		throw e;
+	}
+
+	try {
+		if (!regex_match(s, sm, re))
+			return false;
+
+		for (unsigned i=0; i<sm.size(); i++) {
+			char buffer [33];
+			sprintf(buffer, "%d", i);
+			results[buffer] = sm[i];
+		}
+	}
+	catch (regex_error& e){
+		cout << "tools::matches: regex_match returned error code: `";
+		cout << e.code() << "` when called with s: `" << s;
+		cout << "` and expression: `" << str_re << "`\n";
+		return false;
+	}
+
+	return true;
+}
+
+void tools::test_matches() {
+
+	map<string, string> m;
+  	if (matches(m, "subject", "(sub)(.*)") == false)
+		cout << "test failed!, first matches call returned false!\n";
+
+	if (m["0"] != "subject")
+		cout << "test failed!, match 0 [" << m["0"] << "] did not match!\n";
+
+	if (m["1"] != "sub")
+		cout << "test failed!, match 1 [" << m["1"] << "] did not match!\n";
+
+	if (m["2"] != "ject")
+		cout << "test failed!, match 2 [" << m["2"] << "] did not match!\n";
+
+	if (matches(m, "a   ", "^(\\w+)\\s+(\\w*)$") == false)
+		cout << "test failed!, second matches call returned false!\n";
+
+	if (m["0"] != "a   ")
+		cout << "test failed!, match 0 [" << m["0"] << "] did not match!\n";
+
+	if (m["1"] != "a")
+		cout << "test failed!, match 1 [" << m["1"] << "] did not match!\n";
+
+	if (m["2"] != "")
+		cout << "test failed!, match 2 [" << m["2"] << "] did not match!\n";
 }
--- src/tools.hpp
+++ src/tools.hpp
@@ -1,18 +1,22 @@
-#ifndef _TOOLS
-#define _TOOLS
-
-#include <unordered_map>
-
 // tools.hpp
 // utility functions for mc
 //
 // Created by Daniel Kozitza
 //
 
+#ifndef _TOOLS
+#define _TOOLS
+
+#include <map>
+#include <unordered_map>
+
 using namespace std;
 
 namespace tools {
 	void get_vfnmake_conf(unordered_map<string, string>& config);
+	bool rm_ending_pair(string pair, string&s, int rm_number_pairs = 1);
+	bool matches(map<string, string>& results, string s, string regex);
+	void test_matches();
 }
 
 #endif
